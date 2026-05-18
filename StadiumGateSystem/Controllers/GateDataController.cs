bb5dd6cda42420c304b0263471b5e887dd377288
Evan <evanross@optusnet.com.au>
2026-05-18 21:01:53 +1000

    Added Search and Delete methods

--- StadiumGateSystem/Controllers/GateDataController.cs
+++ StadiumGateSystem/Controllers/GateDataController.cs
@@ -38,6 +38,52 @@ namespace StadiumGateSystem.Controllers
             return gateData;
         }
 
+        // GET api/<GateDataController>/5
+        [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<SearchResult>>> Search(string? gate, string? type, DateTime? startDateTime, DateTime? endDateTime)
+        {
+            //1) get all the gate data from the database
+            var gateData = await _context.Gates.ToListAsync();
+            if (gateData.Count == 0) return new List<SearchResult>();
+
+            //2)  then filter it on the search criteria
+            if (!string.IsNullOrEmpty(gate))
+            {
+                gateData = gateData.Where(g => g.Gate.Equals(gate, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+            if (!string.IsNullOrEmpty(type))
+            {
+                gateData = gateData.Where(g => g.Type.Equals(type, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+            if (startDateTime.HasValue)
+            {
+                gateData = gateData.Where(g => g.Timestamp >= startDateTime.Value).ToList();
+            }
+            if (endDateTime.HasValue)
+            {
+                gateData = gateData.Where(g => g.Timestamp <= endDateTime.Value).ToList();
+            }
+
+            // return an empty array as there is no error, just no records
+            if (!gateData.Any())
+            {
+                return new List<SearchResult>();
+            }
+            //3) Group the gate and type records and sum the number of people
+            List<SearchResult> searchResults =
+            [
+                .. gateData.GroupBy(g => new { g.Gate, g.Type })
+                    .Select(g => new SearchResult
+                    {
+                        Gate = g.Key.Gate,
+                        Type = g.Key.Type,
+                        NumberOfPeople = g.Sum(x => x.NumberOfPeople)
+                    }),
+            ];
+
+            return Ok(searchResults);
+        }
+
         // POST api/<GateDataController>
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] GateData gateData)
@@ -47,21 +93,78 @@ namespace StadiumGateSystem.Controllers
                 return BadRequest();
             }
 
-            _context.Gates.Add(gateData);
-            await _context.SaveChangesAsync();
+            try
+            {
+                _context.Entry(gateData).State = EntityState.Added;
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while adding the gate data.");
+            }
+            finally
+            {
+                _context.Entry(gateData).State = EntityState.Detached;
+            }
+
             return CreatedAtAction(nameof(Get), new { id = gateData.Id }, gateData);
         }
 
         // PUT api/<GateDataController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(int id, [FromBody] GateData gateData)
         {
+            if (id != gateData.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(gateData).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                if (!GateDataExists(id))
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while updating the gate data.");
+            }
+            finally
+            {
+                _context.Entry(gateData).State = EntityState.Detached;
+            }
+
+            return Ok();
+        }
+
+        private bool GateDataExists(int id)
+        {
+            return _context.Gates.Any(e => e.Id == id);
         }
 
         // DELETE api/<GateDataController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
+            int result = 0;
+            try
+            {
+                _context.Entry(new GateData { Id = id }).State = EntityState.Deleted;
+                result = await _context.SaveChangesAsync();
+                // NotFound() Nothing deleted
+                // NoContent() Success (204)
+                return result > 0 ? NoContent() : NotFound();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while deleting the gate data.");
+            }
         }
     }
 }
---
+++ StadiumGateSystem/Models/SearchResult.cs
@@ -0,0 +1,9 @@
+﻿namespace StadiumGateSystem.Models
+{
+    public class SearchResult
+    {
+        public string? Gate { get; set; }
+        public string? Type { get; set; }
+        public int? NumberOfPeople { get; set; }
+    }
+}
